f5566cacb9b6279afc8de151d7167063f8fb2b12
rajeevmohan <rajeevmohan@geekyants.com>
2023-02-22 13:56:35 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import ProgressBar from "./components/ProgressBar";
 
 function App() {
+  const [task, setTask] = useState(0);
+  const [taskCompleted, setTaskCompleted] = useState(0);
+  const color = "#000";
+  const addTask = () => {
+    console.log(task);
+    setTask((task) => task + 1);
+  };
+  const addCompletedTask = () => {
+    console.log(taskCompleted);
+    setTaskCompleted((taskCompleted) => taskCompleted + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <ProgressBar
+        count={50}
+        totalTask={task}
+        completedTask={taskCompleted}
+        color={color}
+      ></ProgressBar>
+      <div className="flex gap-7 justify-center items-center py-2">
+        <button
+          onClick={addTask}
+          className="bg-slate-100 text-slate-500 px-2 rounded py-1"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Add Task
+        </button>
+      </div>
+      <div className="flex gap-7 justify-center items-center">
+        <button
+          className="bg-blue-100 text-blue-700 px-2 rounded py-1"
+          onClick={addCompletedTask}
+        >
+          Add complete Task
+        </button>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/ProgressBar.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+
+function Separator(props) {
+  console.log(props);
+
+  let fill = (props.Len / props.task) * props.taskCompleted;
+  let fillcolor = props.fillColor ? `bg-[${props.fillColor}]` : `bg-blue-500`;
+  let color = props.index < fill ? fillcolor : "bg-slate-300";
+  // console.log(fillcolor);
+  console.log("fill" + fill);
+  // console.log(props.index < fill);
+  console.log(color);
+
+  return (
+    <div
+      style={{
+        transform: `rotate(${props.turns}turn)`,
+        display: "flex",
+        position: "absolute",
+        justifyContent: "start",
+        width: "100%",
+        height: "0.3rem",
+      }}
+      key={props.index}
+    >
+      <div
+        className={`w-[10%] transition-all  ease-in duration-200 ${
+          props.index < fill ? "bg-blue-500" : "bg-slate-300"
+        }`}
+      ></div>
+    </div>
+  );
+}
+
+function ProgressBar({ count, totalTask, completedTask, color }) {
+  const LEN = count;
+  const arr = new Array(LEN).fill(0);
+  const task = totalTask;
+  const taskCompleted = completedTask;
+  const turns = 1 / LEN;
+
+  return (
+    <div>
+      {/* <div className="h-24 w-24 bg-[#015a9e]"></div> */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "1rem",
+        }}
+        // className="flex justify-center items-center py-20"
+      >
+        <div
+          style={{
+            transform: "rotate(90deg)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+            borderRadius: "50%",
+            borderColor: "#CBD5E1",
+            // backgroundColor:"red",
+            width: "200px",
+            height: "200px",
+          }}
+          className=""
+        >
+          {arr.map((val, index) => {
+            return (
+              <Separator
+                turns={index * turns}
+                index={index}
+                task={task}
+                taskCompleted={taskCompleted}
+                Len={LEN}
+                fillColor={color}
+              />
+            );
+          })}
+          {parseInt(taskCompleted) <= parseInt(task) && (
+            <div className=" text-slate-300 text-2xl font-bold p-0 -rotate-90">
+              <span className="text-6xl z-10 text-blue-600 font-extrabold">
+                {taskCompleted}
+              </span>
+              /{task}
+            </div>
+          )}
+          <span className=" text-slate-400 text-2xl p-0  -rotate-90">
+            {parseInt(taskCompleted) >= parseInt(task) ? "Done" : "Task"}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProgressBar;
